fa76dd53e863fcfe4c3d0481f834d1eee661adc6
Samuelikz <samuel.silva@a.ficr.edu.br>
2021-06-08 19:54:07 -0300

    Adicionada classe app controler responsavel po funções de utilização da api

--- petitto_app/Tela_Usuarios.cs
+++ petitto_app/Tela_Usuarios.cs
@@ -39,9 +39,9 @@ namespace petitto_app
 
         private void Tela_Usuarios_Load(object sender, EventArgs e)
         {
-            var users = ListarUsers.BuscarUsers();
+            var users = app_controller.BuscarUsers();
 
-            foreach (var item in from ListarUsers user in users ?? new ListarUsers<string>()
+            foreach (var item in from app_controller user in users ?? new ListarUsers<string>()
                                  let item = new ListViewItem(new[] { user.id.ToString(), user.cpf.ToString(), user.nome.ToString(), user.email.ToString(), user.telefone.ToString() })
                                  select item)
             {
@@ -60,7 +60,7 @@ namespace petitto_app
         }
     }
 
-    internal class ListarUsers<T> : List<ListarUsers>
+    internal class ListarUsers<T> : List<app_controller>
     {
     }
 }
---
+++ petitto_app/app_controller.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace petitto_app
+{
+    class app_controller
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string cpf { get; set; }
+        public string telefone { get; set; }
+        public string nome_pet { get; set; } // Verificar como virar da api, nome pet
+        public string raca { get; set; }
+        public string sexo { get; set; }
+        public string idade { get; set; }
+        public string urlimage { get; set; }
+        public string categoria { get; set; }
+        public string descrincao { get; set; }
+
+        public static List<app_controller> BuscarPets()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+
+                string json = webClient.DownloadString("https://json-serverikz.herokuapp.com/pets"); // Endereço da [API]
+
+                var pets = JsonConvert.DeserializeObject<List<app_controller>>(json); // Transformar em Objeto
+
+                return pets;
+            }
+            catch (Exception ex)
+            {
+                string message = "Não foi Possivel Carregar os Dados \n" + "Erro: " + ex.Message;
+                string title = "Algo Deu Errado";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+
+                return null;
+            }
+        }
+
+        public static List<app_controller> BuscarUsers()
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string json = webClient.DownloadString("https://json-serverikz.herokuapp.com/users"); // Endereço da [API]
+
+                var users = JsonConvert.DeserializeObject<List<app_controller>>(json); // Transformar em Objeto
+                return users;
+            }
+            catch (Exception ex)
+            {
+                string message = "Não foi Possivel Carregar os Dados \n" + "Erro: " + ex.Message;
+                string title = "Algo Deu Errado";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result = MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+
+                return null;
+            }
+        }
+
+        //-------------------------------------------/Função post ???/---------------------------------------------------//
+
+        //public static List<app_controller> Cadastro()
+        //{
+        //    WebRequest newUser = WebRequest.Create("https://json-serverikz.herokuapp.com/users");
+        //    WebRequest newPet = WebRequest.Create("https://json-serverikz.herokuapp.com/pets");
+        //    newUser.Method = "post";
+        //    newPet.Method = "post";
+        //    newUser.ContentType = "application/json, charset=UTF-8";
+        //    newPet.ContentType = "application/json, charset=UTF-8";
+
+
+        //    string NewUser = {  };
+
+        //    return null;
+        //}
+    }
+}
